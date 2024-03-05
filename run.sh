71fd26f555446f411fd52d4854b3e4f78fc9d3ea
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-03-05 07:37:58 -0500

    fix run

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build . -t voxpopulidownloader
-docker run -it -e ROOT=/downloaded -v $(pwd)/downloaded:/downloaded voxpopulidownloader
+docker run -it -e ROOT=/downloaded -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e S3_ENDPOINT=$S3_ENDPOINT -e S3_BUCKET=$S3_BUCKET -v $(pwd)/downloaded:/downloaded voxpopulidownloader
