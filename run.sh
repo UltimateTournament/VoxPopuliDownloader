9cfb30cfcfa7158c3ddb52e27b8ee4263cfb960d
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-03-04 21:14:04 -0500

    it

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build . -t voxpopulidownloader
-docker run -i -e ROOT=/downloaded -v $(pwd)/downloaded:/downloaded voxpopulidownloader
+docker run -it -e ROOT=/downloaded -v $(pwd)/downloaded:/downloaded voxpopulidownloader
